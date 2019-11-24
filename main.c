974291bb097e4ba99bd42b62eb9803b549ebbc13
ck0063 <57468962+ck0063@users.noreply.github.com>
2019-11-23 19:35:23 -0600

    Update main.c
    everything in the old userInput function was bad. using a character array and simply returning the first index of that array makes things easier. we can validate the input as-needed in the code using switch statements

--- main.c
+++ main.c
@@ -1,29 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <ctype.h>
+#include <string.h>
 
-int userInput();
+#define SIZE 1000
+
+char userInput();
 
 int main(){
-    //int i;
 
-    printf("user input is %d\n", userInput());
+    printf("user input is %c\n", userInput());
 
 	return 0;
 }
 
-//do NOT commit to master until fixed
-int userInput(){ //this function does NOT work as-is. invalid input starts infinite loop. not sure why
-    int num, i;
-    char term;
-    do{
-        if(scanf(" %d%c", &num, &term) != 2 || term != '\n'){
-            printf("failure\n");
-            i = 0;
-        }
-        else{
-            i = 1;
-            return num;
-        }
-    }while(i != 1);
+char userInput(){ //taking input as a single character prevents most bad-ness. check the character externally as needed when reading user input.
+    char term[SIZE];
+
+    scanf("%c", &term);
+
+    return term[0];
 }

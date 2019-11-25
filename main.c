4d77050bbc77e79105c83aa6ceec7de8466563a4
ck0063 <57468962+ck0063@users.noreply.github.com>
2019-11-25 04:37:55 -0600

    Update main.c
    added function to get plural input for item IDs. also added the prototype and basic format for the addItem function, and rearranged program structure a tad.

--- main.c
+++ main.c
@@ -6,44 +6,31 @@
 
 #define SIZE 1000
 
-struct item{
+struct item{ //honestly not sure what this is gonna get used for. seemed like a good idea at the time
     char name[100];
     int quantity, id;
     float price;
 };
 
-char singleUserInput();
 void startMenu();
 void checkoutMenu();
 void itemLookup();
+void addItem(int);
+char singleUserInput();
+int pluralUserInput();
 
 int main(){
-    printf("Welcome to the CS102 Grocery Store Kiosk! Input the number of the task you wish to do. Make your selection now!\n");
+    printf("Welcome to the CS102 Grocery Store Kiosk!\n");
     startMenu();
 
-    return 0;
-}
-
-char singleUserInput(){ //taking input as a string literal prevents most bad-ness.
-    char term[SIZE];
-    int result;
-
-    result = scanf(" %s", term);
-
-    if (strlen(term) > 1){ //if user inputs more than one character, just return something that will trigger the function recall
-        if (result == EOF){ //this is the only method i could find to save our souls from someone inputting the EOF character. we just have to say bye-bye and shut it all down.
-            exit(0);
-        }
-        term[0] = '\0';
-    }
-    return term[0];
+	return 0;
 }
 
 void startMenu(){
     char input;
     int flag = 0;
 
-    printf("\n1. Start checkout\n2. Look up item\n\n");
+    printf("\n-----Start menu-----\n\nInput the number of the task you wish to do.\n\n1. Start checkout\n2. Look up item\n\n");
     do{
         input = singleUserInput();
         switch(input){
@@ -54,7 +41,7 @@ void startMenu(){
 
             case '2':
                 flag = 1;
-                //itemLookup();
+                itemLookup();
                 break;
 
             default:
@@ -72,7 +59,7 @@ void checkoutMenu(){
         switch(input){
             case '1': //add item
                 flag = 1;
-
+                addItem(pluralUserInput());
                 break;
 
             case '2': //remove item
@@ -86,8 +73,8 @@ void checkoutMenu(){
                 break;
             case '4': //return to start
                 flag = 1;
-                startMenu();
-                break;
+                startMenu(); //this exposes us to dangerous overflow conditions, but we probably dont have to worry about it.
+                break; //the potentially-recursive function calling should all collapse at the end, so it's probably fine.
 
             default:
                 printf("That isn't valid input. Please try again: ");
@@ -99,16 +86,16 @@ void itemLookup(){
     FILE *listPtr;
     char print[SIZE];
 
-    listPtr = fopen("items.txt", "r+");
-    if(listPtr == NULL){
+    listPtr = fopen("items.txt", "r+"); //arbitrary filename for personal testing
+    if(listPtr == NULL){ //check file open success
         puts("We're sorry, but the item lookup system is experiencing technical difficulties. Seek assistance from staff.");
     }
     else{
-        int i = 0;
+        int i;
         puts("");
-        while(!feof(listPtr)){
-            fgets(print, SIZE, listPtr);
-            for(i = 0; i < SIZE - 1; i++){
+        while(!feof(listPtr)){ //until end of file,
+            fgets(print, SIZE, listPtr); //read in all data up to SIZE bytes to the print array,
+            for(i = 0; i < SIZE - 1; i++){ //and printf the print array.
                 if(print[i] == '\0'){
                     break;
                 }
@@ -117,4 +104,50 @@ void itemLookup(){
         }
     }
     fclose(listPtr);
+}
+
+void addItem(int input){
+    while(input == 0){
+        printf("That item ID could not be found. Please try again: ");
+        input = pluralUserInput();
+    }
+
+    //need file format to continue
+
+}
+
+char singleUserInput(){ //taking input as a string literal prevents most bad-ness.
+    char term[SIZE];
+    int result;
+
+    result = scanf(" %s", term);
+
+    if(strlen(term) > 1){ //if user inputs more than one character, just return something that will trigger the function recall
+        if(result == EOF){ //this is the only method i could find to save our souls from someone inputting the EOF character. we just have to say bye-bye and shut it all down.
+            exit(0);
+        }
+        term[0] = '\0';
+    }
+    return term[0];
+}
+
+int pluralUserInput(){
+    char term[SIZE];
+    int eofcheck, result, i;
+
+    eofcheck = scanf(" %s", term);
+
+    if(strlen(term) != 4){
+        if(eofcheck == EOF){
+            exit(0);
+        }
+        for(i = 0; i < 4; i++){ //arbitrarily writing garbage to the first few bytes if the length isnt 4
+            term[i] = '\0';
+        }
+    }
+    else{
+        result = atoi(term); //function from stdlib to convert string to int
+    }
+    printf("%d\n", result);
+    return result; //returns 0 in case of wrong number of digits OR non-digits. use 0 as function recall flag
 }
